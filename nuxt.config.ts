0e28b5d5b357d3c5d2ff53f3111b57a156cd0a9c
nathanbc46 <nathanbc46@gmail.com>
2025-10-30 16:07:24 +0700

    feat: Add runtime configuration for public base URL

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,10 +2,15 @@
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineNuxtConfig({
+  runtimeConfig: {
+    public: {
+      baseURL: process.env.NUXT_PUBLIC_BASE_URL || 'http://localhost:3000',
+    },
+  },
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   modules: ['@nuxt/eslint', '@nuxt/image', '@nuxt/ui'],
-   css: ['~/assets/css/main.css'],
+  css: ['~/assets/css/main.css'],
   vite: {
     plugins: [
       tailwindcss(),
