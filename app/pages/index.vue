7ea7aab698accf0856dcf76335ca45e5687a0abc
nathanbc46 <nathanbc46@gmail.com>
2025-11-04 16:32:21 +0700

    refactor: change stats to computed property for better reactivity and readability

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -2,38 +2,36 @@
 
 const { data, pending, error } = await useFetch('/api/dashboard/sales')
 
-const stats = ref([
-  {
-    icon: 'i-lucide-trending-up',
-    title: 'ยอดขายวันนี้ ['+ (data.value?.today ? 
-                          formatDate(new Date(data.value.today.dateStart))
-                          : '') +']',
-    value: data.value?.today.total.toLocaleString('th-TH', { style: 'currency', currency: 'THB', maximumFractionDigits: 0 }) ?? 0,
-    variation: data.value?.today.growthPercent ?? 0
-  },
-  {
-    icon: 'i-lucide-chart-line',
-    title: 'ยอดขายเดือนนี้ ['+ (data.value?.month ? 
-                          formatMonth(new Date(data.value.month.dateStart))
-                          : '') +']',
-    value: data.value?.month.total.toLocaleString('th-TH', { style: 'currency', currency: 'THB', maximumFractionDigits: 0 }) ?? 0,
-    variation: data.value?.month.growthPercent ?? 0
-  },
-  {
-    icon: 'i-lucide-chart-scatter',
-    title: 'ยอดขายเฉลี่ยต่อวันในเดือนนี้',
-    value: data.value?.month.averageDailySales.toLocaleString('th-TH', { style: 'currency', currency: 'THB', maximumFractionDigits: 1 }) ?? 0,
-    variation: data.value?.month.growthPercent ?? 0
-  },
-  {
-    icon: 'i-lucide-chart-no-axes-combined',
-    title: 'ยอดขายปีนี้ ['+ (data.value?.year ? 
-                          formatYear(new Date(data.value.year.dateStart))
-                          : '') +']',
-    value: data.value?.year.total.toLocaleString('th-TH', { style: 'currency', currency: 'THB', maximumFractionDigits: 0 }) ?? 0,
-    variation: data.value?.year.growthPercent ?? 0
-  },
-])
+const stats = computed(() => {
+  if (!data.value) return []
+
+  return [
+    {
+      icon: 'i-lucide-trending-up',
+      title: `ยอดขายวันนี้ [${formatDate(new Date(data.value.today.dateStart))}]`,
+      value: data.value.today.total.toLocaleString('th-TH', { style: 'currency', currency: 'THB', maximumFractionDigits: 0 }),
+      variation: data.value.today.growthPercent ?? 0
+    },
+    {
+      icon: 'i-lucide-chart-line',
+      title: `ยอดขายเดือนนี้ [${formatMonth(new Date(data.value.month.dateStart))}]`,
+      value: data.value.month.total.toLocaleString('th-TH', { style: 'currency', currency: 'THB', maximumFractionDigits: 0 }),
+      variation: data.value.month.growthPercent ?? 0
+    },
+    {
+      icon: 'i-lucide-chart-scatter',
+      title: 'ยอดขายเฉลี่ยต่อวันในเดือนนี้',
+      value: data.value.month.averageDailySales.toLocaleString('th-TH', { style: 'currency', currency: 'THB', maximumFractionDigits: 1 }),
+      variation: data.value.month.growthPercent ?? 0
+    },
+    {
+      icon: 'i-lucide-chart-no-axes-combined',
+      title: `ยอดขายปีนี้ [${formatYear(new Date(data.value.year.dateStart))}]`,
+      value: data.value.year.total.toLocaleString('th-TH', { style: 'currency', currency: 'THB', maximumFractionDigits: 0 }),
+      variation: data.value.year.growthPercent ?? 0
+    }
+  ]
+})
 
 
 </script>
