36ecbd271c011a7483f8543709ac1219561224a3
nathanbc46 <nathanbc46@gmail.com>
2025-11-14 17:07:01 +0700

    feat: enhance date formatting functions with additional plugins for improved date comparisons

--- app/utils/dateFormat.ts
+++ app/utils/dateFormat.ts
@@ -1,13 +1,23 @@
 import dayjs from 'dayjs'
+import isSameOrAfter from 'dayjs/plugin/isSameOrAfter'
+import isSameOrBefore from 'dayjs/plugin/isSameOrBefore'
 import 'dayjs/locale/th.js'  // โหลด locale ไทย
+
+dayjs.extend(isSameOrAfter)
+dayjs.extend(isSameOrBefore)
 
 export function formatDate(date: Date) {
     dayjs.locale('th') // ตั้งค่าให้ใช้ภาษาไทย
     return dayjs(date).format('DD MMM YYYY')
 }
 
-export function formatDateTime(date: Date) {
+export function formatDateTime(date: Date, todayText = 'Today') {
     dayjs.locale('th') // ตั้งค่าให้ใช้ภาษาไทย
+
+    if (todayText === 'Today' && dayjs(date).isSame(dayjs(), 'day')) {
+        return todayText+' '+dayjs(date).format('HH:mm')
+    }
+
     return dayjs(date).format('DD/MMM/YYYY HH:mm')
 }
 
