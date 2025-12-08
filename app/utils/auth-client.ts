94a7c5a61f5c1dbe0f4d5da7106533f1ea497142
nathanbc46 <nathanbc46@gmail.com>
2025-12-08 14:05:16 +0700

    feat: Implement authentication client and add `NUXT_AUTH_URL` to docker-compose.

--- app/utils/auth-client.ts
+++ app/utils/auth-client.ts
@@ -3,7 +3,7 @@ import { adminClient } from "better-auth/client/plugins"
 
 export const authClient = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: import.meta.client ? (process.env.BETTER_AUTH_URL || "http://localhost:3000") : "http://localhost:3000",
+    baseURL: import.meta.client ? (process.env.BETTER_AUTH_URL || "http://localhost:3000") : (process.env.NUXT_AUTH_URL || process.env.BETTER_AUTH_URL || "http://localhost:3000"),
     plugins: [
         adminClient()
     ]
